d3e1ff4d82ab0fbd2bfbd677c98d8d0b6e4f377b
Nascimento-Hebo <nascimentohebo2@gmail.com>
2026-05-10 14:28:00 -0300

    feat: nova funcionalidade

---
+++ main.js
@@ -0,0 +1,16 @@
+/*listagem 
+Caminhao preto
+
+Caminhao Castanho
+
+Caminhao Azul
+
+*/
+
+var CPreto = 2;
+var Ccast = 1;
+var CAzul = 2;
+
+var avisao = CPreto + Ccast + CAzul;
+
+console.log(``);
