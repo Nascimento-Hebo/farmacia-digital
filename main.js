2230712b6fb1fd6583d8834de093a1f92c7b249c
Nascimento-Hebo <nascimentohebo2@gmail.com>
2026-05-12 10:10:35 -0300

    Refactor: fiz uma melhoria interna

--- main.js
+++ main.js
@@ -7,14 +7,26 @@ Caminhao Castanho
 Caminhao Azul
 
 */
-
+//inserindo um texto no paragráfo
 var CPreto = 2;
 var Ccast = 1;
 var CAzul = 2;
-
 var avisao = CPreto + Ccast + CAzul;
-
-console.log(``);
+console.log(avisao);
 
 console.log("Olá, mundo!");
+console.log("olá, estou aqui!");
+console.log(1 + 2);
+
+var nome, sobrenome, nomeCompleto;
+nome = "Nascimento";
+sobrenome = "Hebo";
+nomeCompleto = nome + "   " + sobrenome;
+
+//alert(nomeCompleto)
+
+
+
+
+
 
